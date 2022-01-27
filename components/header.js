17b98a8eb6e3098e0b0265197765f7e0aac1cc0e
Ayton MacEachern <8theVirulent@gmail.com>
2022-01-26 19:24:29 -0700

    refactor SASS code

--- components/header.js
+++ components/header.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
-import BtnPrimaryRoundedFull from "./BtnPrimaryRoundedFull";
 
 export default function Header() {
     return (
+        <>
+        <Head>
+            <html data-theme="studio13" />
+        </Head>
         <header className="component-header">
         <div className="nav-div-1">
             <span className="nav-span-1"><img className="leftlogo" src="/logo.png" /></span>
             <span className="nav-span-2"><i className="material-icons right">menu</i></span>
         </div>
         </header>
+        </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-    purge: [
+    purge: {
         // This is not present inside the default configuration
         // but it's good to build your production application
         // Read more about this here: https://tailwindcss.com/docs/installation#building-your-css
-        './pages/**/*.js',
-    ],
+        content: ['./pages/**/*.js'],
+        options: {
+            safelist: [
+                /data-theme$/
+            ]
+        },
+    },
     darkMode: true,
     theme: {
         extend: {},
@@ -12,5 +17,44 @@ module.exports = {
     variants: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('daisyui'),
+    ],
+    daisyui: {
+        themes: [
+            {
+                'studio13': {                          /* your theme name */
+                    'primary': '#a991f7',           /* Primary color */
+                    'primary-focus': '#8462f4',     /* Primary color - focused */
+                    'primary-content': '#ffffff',   /* Foreground content color to use on primary color */
+
+                    'secondary': '#f6d860',         /* Secondary color */
+                    'secondary-focus': '#f3cc30',   /* Secondary color - focused */
+                    'secondary-content': '#ffffff', /* Foreground content color to use on secondary color */
+
+                    'accent': '#37cdbe',            /* Accent color */
+                    'accent-focus': '#2aa79b',      /* Accent color - focused */
+                    'accent-content': '#ffffff',    /* Foreground content color to use on accent color */
+
+                    'neutral': '#3d4451',           /* Neutral color */
+                    'neutral-focus': '#2a2e37',     /* Neutral color - focused */
+                    'neutral-content': '#ffffff',   /* Foreground content color to use on neutral color */
+
+                    'base-100': '#ffffff',          /* Base color of page, used for blank backgrounds */
+                    'base-200': '#f9fafb',          /* Base color, a little darker */
+                    'base-300': '#d1d5db',          /* Base color, even more darker */
+                    'base-content': '#1f2937',      /* Foreground content color to use on base color */
+
+                    'info': '#2094f3',              /* Info */
+                    'success': '#009485',           /* Success */
+                    'warning': '#ff9900',           /* Warning */
+                    'error': '#ff5724',             /* Error */
+                },
+            },
+            'dark',
+            'light',
+            'CMYK',
+            'synthwave'
+        ],
+    },
 }
